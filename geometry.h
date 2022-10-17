e43479bfc236f75b83b266a170fdda416bf8e527
sdoering01 <57443123+sdoering01@users.noreply.github.com>
2022-10-17 15:23:10 +0200

    add include guards to geometry header

--- geometry.h
+++ geometry.h
@@ -1,3 +1,6 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
 typedef struct vec2_t {
     float x;
     float y;
@@ -8,3 +11,5 @@ vec2_t vec2_sub(vec2_t v1, vec2_t v2);
 vec2_t vec2_scale(vec2_t v, float factor);
 float vec2_abs(vec2_t v);
 vec2_t vec2_norm(vec2_t v);
+
+#endif // GEOMETRY_H
