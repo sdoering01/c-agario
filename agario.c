81c9bd4667bb8773bf2f5d5b95a723666a8e12ec
sdoering01 <57443123+sdoering01@users.noreply.github.com>
2022-10-28 21:48:31 +0200

    implement player joins

--- agario.c
+++ agario.c
@@ -9,6 +9,7 @@
 #include <errno.h>
 #include <signal.h>
 #include <time.h>
+#include <stdbool.h>
 
 #include "geometry.h"
 #include "protocol.h"
@@ -20,19 +21,26 @@
 #define FIELD_HEIGHT 1000
 #define FIELD_WIDTH 1000
 
+#define START_MASS 10
+
 static char *too_many_connections_message = "too_many_connections";
 
+// TODO: Add way to disconnect players that don't join in time to prevent denial
+// of service attacks
 typedef struct player_t {
     int sock;
     int id;
     int mass;
     char *name;
+    rejoin_token_t rejoin_token;
     vec2_t pos;
     vec2_t target;
+    bool joined;
 } player_t;
 
 typedef struct context_t {
     int epoll_fd;
+    int next_player_id;
     player_t *players[MAX_PLAYERS];
 } context_t;
 
@@ -42,30 +50,35 @@ static vec2_t generate_player_pos() {
     return (vec2_t){x, y};
 }
 
-static player_t *player_new(int sock, int id, char *name) {
-    size_t name_len;
-    player_t *p = malloc(sizeof(player_t));
+static player_t *player_new(int sock) {
+    player_t *p = calloc(1, sizeof(player_t));
     p->sock = sock;
-    p->id = id;
-    p->mass = 10;
-    if (name && (name_len = strlen(name)) <= MAX_PLAYER_NAME_LEN) {
-        p->name = malloc(name_len + 1);
-        strcpy(p->name, name);
-    } else {
-        p->name = malloc(strlen(DEFAULT_PLAYER_NAME) + 1);
-        strcpy(p->name, DEFAULT_PLAYER_NAME);
-    }
-    p->pos = generate_player_pos();
-    p->target = p->pos;
+    p->joined = false;
     return p;
 }
 
 static void player_free(player_t *p) {
-    free(p->name);
+    if (p->name) {
+        free(p->name);
+    }
     free(p);
 }
 
-static int send_all(int sock, char *msg, int msg_len) {
+static int generate_player_id(context_t *ctx) {
+    return ctx->next_player_id++;
+}
+
+static int get_player_count(context_t *ctx) {
+    int count = 0;
+    for (int i = 0; i < MAX_PLAYERS; i++) {
+        if (ctx->players[i] && ctx->players[i]->joined) {
+            count++;
+        }
+    }
+    return count;
+}
+
+static int send_all(int sock, uint8_t *msg, int msg_len) {
     int sent;
     while (msg_len > 0) {
         sent = send(sock, msg, msg_len, 0);
@@ -79,6 +92,38 @@ static int send_all(int sock, char *msg, int msg_len) {
         msg_len -= sent;
     }
     return 0;
+}
+
+static int connect_player(int sock, context_t *ctx) {
+    struct epoll_event event = {};
+    int ret, idx = 0;
+
+    while (idx < MAX_PLAYERS && ctx->players[idx]) {
+        idx++;
+    }
+
+    if (idx == MAX_PLAYERS) {
+        printf("player tried to connect while game was full\n");
+        // TODO: Swap for binary error message once protocol is established
+        send_all(sock, (uint8_t *)too_many_connections_message, strlen(too_many_connections_message));
+        close(sock);
+        return -1;
+    }
+
+    player_t *player = player_new(sock);
+
+    event.events = EPOLLIN;
+    event.data.ptr = player;
+    ret = epoll_ctl(ctx->epoll_fd, EPOLL_CTL_ADD, sock, &event);
+    if (ret == -1) {
+        printf("Error adding player socket to epoll instance, closing socket\n");
+        player_free(player);
+        close(sock);
+    } else {
+        ctx->players[idx] = player;
+    }
+
+    return ret;
 }
 
 static void disconnect_player(player_t *player, context_t *ctx) {
@@ -98,50 +143,105 @@ static void disconnect_player(player_t *player, context_t *ctx) {
     }
 }
 
-static int join_player(int sock, context_t *ctx) {
-    struct epoll_event event = {};
-    int ret, idx = 0;
+static void join_player(player_t *player, join_message_t *msg, context_t *ctx) {
+    char *name = msg->name ? msg->name : DEFAULT_PLAYER_NAME;
+    int name_len = msg->name ? msg->name_length : DEFAULT_PLAYER_NAME_LENGTH;
 
-    while (idx < MAX_PLAYERS && ctx->players[idx]) {
-        idx++;
-    }
-
-    if (idx == MAX_PLAYERS) {
-        printf("Got too many connections");
-        // TODO: Swap for binary error message once protocol is established
-        send_all(sock, too_many_connections_message, strlen(too_many_connections_message));
-        close(sock);
-        return -1;
-    }
-
-    // TODO: Use name provided by player
-    player_t *player = player_new(sock, idx, NULL);
-
-    event.events = EPOLLIN;
-    event.data.ptr = player;
-    ret = epoll_ctl(ctx->epoll_fd, EPOLL_CTL_ADD, sock, &event);
-    if (ret == -1) {
-        printf("Error adding player socket to epoll instance, closing socket\n");
-        free(player);
-        close(sock);
-    } else {
-        ctx->players[idx] = player;
-        // TODO: Send position to player
-    }
-
-    return ret;
+    player->id = generate_player_id(ctx);
+    player->mass = START_MASS;
+    player->name = malloc(name_len + 1);
+    strcpy(player->name, name);
+    // TODO: Generate rejoin token with cryptographic randomness
+    memset(player->rejoin_token, 0, REJOIN_TOKEN_LEN);
+    player->pos = generate_player_pos();
+    player->target = player->pos;
+    player->joined = true;
 }
 
-static void broadcast(char *msg, int msg_len, context_t *ctx) {
+static void send_bytes(uint8_t *buf, int buf_len, player_t *player, context_t *ctx) {
+    if (buf_len <= 0) {
+        return;
+    }
+
+    if (send_all(player->sock, buf, buf_len) == -1) {
+        printf("Error when sending to socket: errno %d -- %s\n", errno, strerror(errno));
+        printf("Closing socket\n");
+        // TODO: Handle disconnects in such a way, that there isn't the risk of
+        // trying to send to a player that was disconnected (e.g. when sending
+        // two messages to a player in during one tick).
+        // This could be done by setting a flag on the player struct and
+        // disconnecting players that have the flag set during each tick.
+        // Sending a message to players that have that flag set should also be
+        // skipped.
+        disconnect_player(player, ctx);
+    }
+}
+
+static void broadcast_bytes(uint8_t *buf, int buf_len, context_t *ctx) {
+    if (buf_len <= 0) {
+        return;
+    }
+
     for (int i = 0; i < MAX_PLAYERS; i++) {
         player_t *player = ctx->players[i];
-        if (player) {
-            if (send_all(player->sock, msg, msg_len) == -1) {
-                printf("Error when sending to socket: errno %d -- %s\n", errno, strerror(errno));
-                printf("Closing socket\n");
-                disconnect_player(player, ctx);
+        if (player && player->joined) {
+            send_bytes(buf, buf_len, player, ctx);
+        }
+    }
+}
+
+static void handle_player_message(uint8_t *recv_buf, uint16_t recv_len, player_t *player, context_t *ctx) {
+    uint8_t send_buf[65535];
+    int send_len;
+
+    generic_message_t *generic_msg = deserialize_message(recv_buf, recv_len);
+    if (generic_msg) {
+        if (!player->joined && generic_msg->message_type == MSG_JOIN) {
+            join_player(player, (join_message_t *)generic_msg, ctx);
+
+            join_ack_message_t join_ack_msg = {};
+            join_ack_msg.message_type = MSG_JOIN_ACK;
+            join_ack_msg.player_id = player->id;
+            memcpy(join_ack_msg.rejoin_token, player->rejoin_token, REJOIN_TOKEN_LEN);
+            send_len = serialize_message(
+                    (generic_message_t *)&join_ack_msg, send_buf, sizeof(send_buf));
+            send_bytes(send_buf, send_len, player, ctx);
+
+            player_join_message_t player_join_msg = {};
+            player_join_msg.message_type = MSG_PLAYER_JOIN;
+            player_join_msg.player_info.player_id = player->id;
+            player_join_msg.player_info.name_length = strlen(player->name);
+            player_join_msg.player_info.name = player->name;
+            send_len = serialize_message(
+                    (generic_message_t *)&player_join_msg, send_buf, sizeof(send_buf));
+            broadcast_bytes(send_buf, send_len, ctx);
+
+            current_players_message_t current_players_msg = {};
+            current_players_msg.message_type = MSG_CURRENT_PLAYERS;
+            int player_count = get_player_count(ctx);
+            current_players_msg.player_count = player_count;
+            current_players_msg.player_infos = calloc(player_count, sizeof(player_info_t));
+            int info_idx = 0;
+            for (int i = 0; i < MAX_PLAYERS; i++) {
+                if (ctx->players[i] && ctx->players[i]->joined) {
+                    current_players_msg.player_infos[info_idx].player_id = ctx->players[i]->id;
+                    current_players_msg.player_infos[info_idx].name_length =
+                        strlen(ctx->players[i]->name);
+                    current_players_msg.player_infos[info_idx].name = ctx->players[i]->name;
+                    info_idx++;
+                }
+            }
+            send_len = serialize_message(
+                    (generic_message_t *)&current_players_msg, send_buf, sizeof(send_buf));
+            send_bytes(send_buf, send_len, player, ctx);
+            free(current_players_msg.player_infos);
+        } else if (player->joined) {
+            switch (generic_msg->message_type) {
+                // TODO: Handle other messages
             }
         }
+
+        message_free(generic_msg);
     }
 }
 
@@ -153,7 +253,7 @@ int main(void) {
     int server_sock, client_sock, epoll_fd, timer_fd, running = 1, event_count, i;
     uint64_t tick_count;
     struct sockaddr_in server_addr;
-    char client_message[2000] = {};
+    uint8_t client_message[2000] = {};
     ssize_t bytes_received;
     struct epoll_event event = {}, events[MAX_EVENTS] = {};
     struct itimerspec interval = {};
@@ -242,32 +342,6 @@ int main(void) {
         return 1;
     }
 
-
-    // TODO: Remove this later on
-    printf("Testing protocol\n");
-
-    uint8_t buf[65535] = {};
-    join_message_t msg;
-    msg.message_type = MSG_JOIN;
-    msg.name_length = 5;
-    msg.name = malloc(6);
-    strncpy(msg.name, "Simon", 6);
-
-    int msg_len = serialize_message((generic_message_t *)&msg, buf, 65535);
-    printf("msg_len: %d\n", msg_len);
-    for (int i = 0; i < msg_len; i++) {
-        printf("%x ", buf[i]);
-    }
-    printf("\n");
-
-    join_message_t *dmsg = (join_message_t *)deserialize_message(buf, msg_len);
-    printf("message_type: %d\n", dmsg->message_type);
-    printf("name_length: %d\n", dmsg->name_length);
-    printf("name: %s\n", dmsg->name);
-    message_free((generic_message_t *)dmsg);
-
-    printf("Finished testing protocol\n");
-
     while (running) {
         event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
         for (i = 0; i < event_count; i++) {
@@ -279,7 +353,7 @@ int main(void) {
                     printf("Couldn't accept connection...\n");
                     continue;
                 }
-                if (join_player(client_sock, &ctx) == -1) {
+                if (connect_player(client_sock, &ctx) == -1) {
                     // In case something gets added after the if statement later
                     continue;
                 }
@@ -293,6 +367,9 @@ int main(void) {
             } else {
                 player_t *player = events[i].data.ptr;
                 printf("player socket ready: %d\n", player->sock);
+                // TODO: Buffer the data in a player-specific buffer or try to
+                // peek the data and look if we can read enough data to get a
+                // message.
                 bytes_received = recv(player->sock, client_message, sizeof(client_message), 0);
                 // Be careful when handling errno, because calls to printf can overwrite it
                 if (bytes_received == -1 && errno != EINTR) {
@@ -307,7 +384,7 @@ int main(void) {
                     continue;
                 }
 
-                broadcast(client_message, bytes_received, &ctx);
+                handle_player_message(client_message, bytes_received, player, &ctx);
             }
         }
     }
